8d8cecabb80ad62f843a5950346f7250c590baea
AbuKaramHasan <abukaramhasan@yahoo.com>
2024-12-26 13:58:36 +0300

    Update main.go

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	http.HandleFunc("/msg", handlers.MsgHandler)
 	http.HandleFunc("/event", handlers.EventHandler)
 	http.HandleFunc("/sse", handlers.SSEHandler)
+	http.HandleFunc("/custom", handlers.CustomHandler)
 
 	go func() {
 		fmt.Println("Starting server on :8080...")
